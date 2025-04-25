8e3316918e7143b757dc82f4e823d2a883969adf
talent386 <tcs202683@gmail.com>
2025-04-25 17:59:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient.DataClassification;
+using System.Data;
+using System.Data.Common;
+
+SqlConnection conexao = new SqlConnection("Server=#;Initial Catalog=#;User Id=#;Password=#;TrustServerCertificate=True;");
+
+  
+        conexao.Open();
+        Console.WriteLine(conexao.State.ToString());
+        SqlCommand command= conexao.CreateCommand();
+         command.CommandText = "teste";
+
+        command.CommandTimeout = 15;
+        command.CommandType = CommandType.StoredProcedure;
+
+                 SqlDataAdapter adapter = new SqlDataAdapter(command);
+                 DataSet DSteste = new DataSet();
+                 adapter.Fill(DSteste);
+
+        conexao.Close();
+              Console.WriteLine(conexao.State.ToString());
+
+
